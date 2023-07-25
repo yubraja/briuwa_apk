7eb1bd0d877750e69d4abf04bd0c7f1f66557798
yubraja <yubrajadhikari987@gmail.com>
2023-07-25 23:43:49 +0545

    changed to make our project clean

--- biruwa_apk/lib/pages/home_page.dart
+++ biruwa_apk/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:biruwa_apk/constant.dart';
 /// Flutter code sample for [BottomNavigationBar].
 
 
@@ -15,23 +16,8 @@ class HomePage extends StatefulWidget {
 class _HomePage
     extends State<HomePage> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-
+ 
+ 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -41,23 +27,33 @@ class _HomePage
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BottomNavigationBar Sample'),
-      ),
+      
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+        child:widgetOptions[_selectedIndex],
       ),
+
       bottomNavigationBar: BottomNavigationBar(
         items:  <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+
+          BottomNavigationBarItem(icon:Image.asset('assets/two_leave.jpg', height: 40,),label: ' '),
+
+
+           BottomNavigationBarItem(
+            icon: Icon(Icons.search_rounded),
+            label: ' ',
           ),
-         BottomNavigationBarItem(icon:Image.asset('assets/two_leave.jpg'),label: ' '),
+
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
+            icon: Icon(Icons.favorite),
+            label: ' ',
           ),
+         
+          
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_checkout_outlined),
+            label: ' ',
+          ),
+         
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
