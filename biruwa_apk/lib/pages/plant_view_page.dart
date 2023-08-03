0fb0a01a2e472034688a1b206a5fc06b2d751922
yubraja <yubrajadhikari987@gmail.com>
2023-08-03 16:38:51 +0545

    after roaming around i got how to solve the problem

--- biruwa_apk/lib/pages/plant_view_page.dart
+++ biruwa_apk/lib/pages/plant_view_page.dart
@@ -8,23 +8,56 @@ class PlantViewPage extends StatefulWidget {
 }
 
 class _PlantViewPageState extends State<PlantViewPage> {
+
+  int selectedIndex=0;
   @override
+
+
   Widget build(BuildContext context) {
     return Container(
       child: 
       Column(
         children: [
           Container(child: Image.asset('assets/girl.png',width: 500,fit: BoxFit.cover,),width: double.infinity,),
+
+
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: Row(children: [
-              buttonCreation('Recommend',0),
-              buttonCreation('Top',1),
-              buttonCreation('Popoular',2),
-              buttonCreation('Indoor',3),
-              buttonCreation('Outdoor',4),
-          
-          
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                
+               GestureDetector(onTap: (){
+                setState(() {
+                  selectedIndex=0;
+                });
+               }, child:Container(
+                alignment: Alignment.center,
+                height: 40,
+                width: 120,
+
+                child:Text('Recommend'),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),
+                color: Color.fromARGB(255, 19, 175, 100)),
+                ),
+               )
+               
+
+
+                
+                
+                
+                
+                
+                
+                
+                
+                //top //popular//indoor //outdoor
+              
+                //   ),
+                     
+                    
+                    
             ],),
           )
 
@@ -36,24 +69,5 @@ class _PlantViewPageState extends State<PlantViewPage> {
     );
   }
 
- Widget buttonCreation(String label,int index) {
 
-  return TextButton(onPressed: (){
-
-    setState(() {
-      
-    });
-    
-    
-  }, child:Text(label),
-  // style: ButtonStyle(
-  //   backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(255, 168,190,198),),
-    
-  //   ),
-    
-  
-  
-  );
-
- }
 }
