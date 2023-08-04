b30a66697a60e3ffac6d7b4fb015ab97db1c2e0b
yubraja <yubrajadhikari987@gmail.com>
2023-08-04 21:46:50 +0545

    working on showcase to show image of each individual

--- biruwa_apk/lib/model/showcase.dart
+++ biruwa_apk/lib/model/showcase.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'plant.dart';
 
 class ShowCase extends StatelessWidget {
-  const ShowCase({super.key});
+   ShowCase({super.key,required this.height,required this.width});
+  
+  double width ;
+  double height ;
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: GridView.builder(itemBuilder:(context, index){
-        
-      },gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),),
+    return Container(
+      height: height*0.3,
+      width: width,
+      
+      child: SingleChildScrollView(
+        child: GridView.builder(
+          itemCount: plant.length,
+          itemBuilder:(context, index){
+          Container(
+            child:Column(
+              children: [
+                Image.asset(plant[index].image),
+                
+                Stack(
+                  children: [
+                    Text(plant[index].name),
+    
+                  ],
+                )
+              ],
+            ),
+          );
+    
+          
+        },gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),),
+      ),
     );
   }
 }
