6953ca452c38f5af619662fd9817b537b075f2f5
yubraja <yubrajadhikari987@gmail.com>
2023-08-04 23:23:12 +0545

    tried name in first page

--- biruwa_apk/lib/model/plant.dart
+++ biruwa_apk/lib/model/plant.dart
@@ -33,11 +33,11 @@ List<Plant> _plant=[
 
   Plant(name: 'Samantha',image: 'assets/images/image_1.png',type: 'Indoor',price:1000,height: '50 to 100', details: 'This is indoor type of plant'),
   Plant(name: 'Angelica',image: 'assets/images/image_2.png',type: 'Indoor',price:800,height: '40 to 80', details: 'This is indoor type of plant'),
-  Plant(name: 'Samantha',image: 'assets/images/image_1.png',type: 'Indoor',price:1000,height: '50 to 100', details: 'This is indoor type of plant'),
-  Plant(name: 'Samantha',image: 'assets/images/image_1.png',type: 'Indoor',price:1000,height: '50 to 100', details: 'This is indoor type of plant'),
-  Plant(name: 'Samantha',image: 'assets/images/image_1.png',type: 'Indoor',price:1000,height: '50 to 100', details: 'This is indoor type of plant'),
-  Plant(name: 'Samantha',image: 'assets/images/image_1.png',type: 'Indoor',price:1000,height: '50 to 100', details: 'This is indoor type of plant'),
-  Plant(name: 'Samantha',image: 'assets/images/image_1.png',type: 'Indoor',price:1000,height: '50 to 100', details: 'This is indoor type of plant'),
+  Plant(name: 'Moth Orchid',image: 'assets/images/image_3.png',type: 'Indoor',price:700,height: '50 to 100', details: 'This is indoor type of plant'),
+  Plant(name: 'Monstera Deliciosa ',image: 'assets/images/plant_4.avif',type: 'Indoor',price:1000,height: '50 to 100', details: 'This is indoor type of plant'),
+  Plant(name: 'Pepper Face',image: 'assets/images/plant5.avif',type: 'Indoor',price:1000,height: '50 to 100', details: 'This is indoor type of plant'),
+  Plant(name: 'Pothos',image: 'assets/images/plant6.avif',type: 'Indoor',price:1000,height: '50 to 100', details: 'This is indoor type of plant'),
+  Plant(name: 'Zz Plant',image: 'assets/images/plant7.avif',type: 'Indoor',price:1000,height: '50 to 100', details: 'This is indoor type of plant'),
 
 ];
 
--- biruwa_apk/lib/pages/plant_view_page.dart
+++ biruwa_apk/lib/pages/plant_view_page.dart
@@ -1,135 +1,198 @@
 import 'package:flutter/material.dart';
-import '../model/plant.dart'; 
+import '../model/plant.dart';
 class PlantViewPage extends StatefulWidget {
-   PlantViewPage({super.key});
+  PlantViewPage({Key? key}) : super(key: key);
 
   @override
   State<PlantViewPage> createState() => _PlantViewPageState();
 }
 
 class _PlantViewPageState extends State<PlantViewPage> {
+  int selectedIndex = 0;
 
-  int selectedIndex=0;
-  
   @override
-
-
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Container(
-      
-      child: 
-      Column(
+      child: Column(
         children: [
-
-          //here image and notification bell icon is shown
           Stack(
             alignment: Alignment.topLeft,
-            
-            children:[ 
-              Container(child: Image.asset('assets/girl.png',width: 500,fit: BoxFit.cover,),width: double.infinity,),
-            Positioned(
-              top: 40,
-              right: 10,
-            
-              child: Container(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),
-                color: Color.fromARGB(255, 25, 132, 80),),
-                child: Icon(Icons.notification_add),
+            children: [
+              Container(
+                child: Image.asset(
+                  'assets/girl.png',
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-          ],),
-
-
+              Positioned(
+                top: 40,
+                right: 10,
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    color: const Color.fromARGB(255, 13, 218, 119),
+                  ),
+                  child: Icon(Icons.notification_add),
+                ),
+              ),
+            ],
+          ),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-
                 buttonProvider('Recommend', 0),
                 buttonProvider('Top', 1),
                 buttonProvider('Popular', 2),
                 buttonProvider('Indoor', 3),
                 buttonProvider('Outdoor', 4),
-
-
-            
-                
-                
-                
-                //top //popular//indoor //outdoor
-              
-                //   ),
-                     
-                    
-                    
-            ],),
+              ],
+            ),
           ),
-
-
-          //here the list of plants are shown
-
           SingleChildScrollView(
             child: Container(
-              height: height*0.38,
-
-              width:width,
+              height: height * 0.38,
+              width: width,
               child: GridView.builder(
                 itemCount: plant.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                itemBuilder: (context, index){
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (context, index) {
                   return Column(
                     children: [
-                      Container(padding: EdgeInsets.all(20),height:height*0.45-200,child: Image.asset(plant[index].image,fit: BoxFit.contain)),
-        
-                   
+                      Stack(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.
+                            symmetric(horizontal: 10, ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+
+                              color: Colors.white,
+                            ),
+                            height: height * 0.45 - 200,
+                            width: width * 0.90,
+                            child: Image.asset(plant[index].image,
+                                fit: BoxFit.cover),
+                          ),
+
+
+
+                          Positioned(
+                            top: 10,
+                            right: 10,
+                            child: Container(
+                              height: 30,
+                              width: 30,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Colors.white,
+                              ),
+                              child: Icon(Icons.favorite_border,
+                                  color: Colors.grey),
+                            ),
+                          ),
+
+
+
+                          Positioned(
+                            bottom: -70,
+                            left: 20,
+                            child: Container(
+                              height: 90,
+                              width: 150,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 5,
+                                    blurRadius: 7,
+                                    offset: Offset(0, 3),
+                                  ),
+                                ],
+                              ),
+                              child:
+                              Column(
+                                children: [
+                                  Text(
+                                    plant[index].name,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: const Color.fromARGB(
+                                          255, 13, 218, 119),
+                                    ),
+                                  ),
+                                  Center(
+                                    
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween
+
+                                      ,
+                                      children: [
+                                        Text(
+                                          plant[index].type,
+                                          style: TextStyle(
+                                            fontSize: 15,
+                                          color: const Color.fromARGB(
+                                          255, 13, 218, 119),                                          ),
+                                        ),
+                                  
+                                        Text(
+                                          'Rs'+plant[index].price.toString(),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
                     ],
                   );
-                }),
+
+                },
+              ),
             ),
-          )
-
-
-
-
-
+          ),
         ],
       ),
     );
   }
 
-  Widget buttonProvider(String label, int index)
-  {
-    
-        return    Padding(
-                 padding: const EdgeInsets.all(10.0),
-                 child: GestureDetector(onTap: (){
-                  setState(() {
-                    selectedIndex=index;
-                  });
-                 }, child:Container(
-                  alignment: Alignment.center,
-                  height: 40,
-                  width: selectedIndex==index?120:80,
-               
-                  child:Text(label),
-                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(50),
-                  color: selectedIndex==index?Color.fromARGB(255, 19, 175, 100):Colors.white,)
-                  ),
-                 ),
-               );
-
-
-
+  Widget buttonProvider(String label, int index) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        child: Material(
+          color: selectedIndex == index
+              ? const Color.fromARGB(255, 13, 218, 119)
+              : Colors.white,
+          borderRadius: BorderRadius.circular(50),
+          child: Container(
+            alignment: Alignment.center,
+            height: 40,
+            width: selectedIndex == index ? 120 : 80,
+            child: Text(label),
+          ),
+        ),
+      ),
+    );
   }
-
-
-
-
-
-
 }
