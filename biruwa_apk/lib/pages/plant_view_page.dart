5f63cd5df671c73a0fa78038b2aeec2943c8e131
yubraja <yubrajadhikari987@gmail.com>
2023-08-07 23:04:25 +0545

    error stuck at text

--- biruwa_apk/lib/pages/plant_view_page.dart
+++ biruwa_apk/lib/pages/plant_view_page.dart
@@ -57,111 +57,109 @@ class _PlantViewPageState extends State<PlantViewPage> {
           ),
           SingleChildScrollView(
             child: Container(
-              height: height * 0.45,
               width: width,
               child: GridView.builder(
                 itemCount: plant.length,
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2),
                 itemBuilder: (context, index) {
-                  return Container(
-                        height: height * 0.45,
-
-                    child: Column(
-                      children: [
-                        Stack(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.
-                              symmetric(horizontal: 10, ),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
+                  return Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.
+                            symmetric(horizontal: 5, ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
                   
+                              color: Colors.white,
+                            ),
+                            height: height * 0.45 - 200,
+                            width: width * 0.90,
+                            child: 
+                            
+                            Image.asset(plant[index].image,
+                                fit: BoxFit.fitHeight,
+                                height: height*0.45-100,),
+                          ),
+                  
+                  
+                  
+                          Positioned(
+                            top: 10,
+                            right: 10,
+                            child: Container(
+                              height: 0,
+                              width: 30,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
                                 color: Colors.white,
                               ),
-                              height: height * 0.45 - 200,
-                              width: width * 0.90,
-                              child: Image.asset(plant[index].image,
-                                  fit: BoxFit.cover),
+                              child: Icon(Icons.favorite_border,
+                                  color: Colors.grey),
                             ),
+                          ),
                   
                   
                   
-                            Positioned(
-                              top: 10,
-                              right: 10,
-                              child: Container(
-                                height: 30,
-                                width: 30,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(50),
-                                  color: Colors.white,
-                                ),
-                                child: Icon(Icons.favorite_border,
-                                    color: Colors.grey),
+                          Positioned(
+                            bottom: -90,
+                            left: 20,
+                            child: Container(
+                              height: 120,
+                              width: 150,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 5,
+                                    blurRadius: 7,
+                                    offset: Offset(0, 3),
+                                  ),
+                                ],
                               ),
-                            ),
-                  
-                  
-                  
-                            Positioned(
-                              bottom: -90,
-                              left: 20,
-                              child: Container(
-                                height: 120,
-                                width: 150,
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.5),
-                                      spreadRadius: 5,
-                                      blurRadius: 7,
-                                      offset: Offset(0, 3),
+                              child:
+                              Column(
+                                children: [
+                                  Text(
+                                    plant[index].name,
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: const Color.fromARGB(
+                                          255, 13, 218, 119),
                                     ),
-                                  ],
-                                ),
-                                child:
-                                Column(
-                                  children: [
-                                    Text(
-                                      plant[index].name,
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween
+                  
+                                    ,
+                                    children: [
+                                      Text(
+                                        plant[index].type,
+                                        style: TextStyle(
+                                          fontSize: 15,
                                         color: const Color.fromARGB(
-                                            255, 13, 218, 119),
+                                        255, 13, 218, 119),                                          ),
                                       ),
-                                    ),
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.spaceBetween
-                  
-                                      ,
-                                      children: [
-                                        Text(
-                                          plant[index].type,
-                                          style: TextStyle(
-                                            fontSize: 15,
-                                          color: const Color.fromARGB(
-                                          255, 13, 218, 119),                                          ),
-                                        ),
-                                    
-                                        Text(
-                                          'Rs'+plant[index].price.toString(),
-                                        )
-                                      ],
-                                    ),
-                                    
-                                  ],
-                                ),
+                                  
+                                      Text(
+                                        'Rs'+plant[index].price.toString(),
+                                      )
+                                    ],
+                                  ),
+                                  
+                                ],
                               ),
                             ),
-                          ],
-                        ),
-                        SizedBox(height: 20),
-                      ],
-                    ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                    ],
                   );
 
                 },
